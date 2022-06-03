8174ecfe2cbd98673c5ddc29d4ba76925565beb9
vedGit77 <104839104+vedGit77@users.noreply.github.com>
2022-06-03 12:42:51 +0530

    Update 13 Median of a Stream.cpp

--- 13 Median of a Stream.cpp
+++ 13 Median of a Stream.cpp
@@ -1,3 +1,36 @@
+//Remember...Median is always on a SORTED array
+
+// Question:
+//Given an input stream of N integers. 
+//The task is to insert these numbers into a new stream and find the median of the stream formed by each insertion of X to the new stream.
+//eg. the given array/stream is: {25,7,10,15,20}, then O/P is {25,16,10,12.5,15}
+
+
+//Naive sol: say given array size =n
+//	   : so for i=1 till i=n, create array of size = i
+//         : sort the array....now simply get median of the array, and print/store it
+//         : so we continue this for all i's from 1 to n
+//	   : Time= O(n*n) = O(n^2)
+
+
+
+//Efficient soln: Time= O(n* log n)
+
+// 1. Create two heaps. One max heap to maintain elements of lower half and one min heap to maintain elements of higher half at any point of time..
+// 2. Take initial value of median as 0.
+// 3. For every newly read element, insert it into either max heap or min-heap and calculate the median based on the following conditions: 
+// a. If the size of max heap is greater than the size of min-heap and the element is less than the previous median 
+//    then pop the top element from max heap and insert into min-heap and insert the new element to max heap else insert the new element to min-heap. 
+//    Calculate the new median as the average of top of elements of both max and min heap.
+// b. If the size of max heap is less than the size of min-heap and the element is greater than the previous median 
+//    then pop the top element from min-heap and insert into the max heap and insert the new element to min heap 
+//    else insert the new element to the max heap. Calculate the new median as the average of top of elements of both max and min heap.
+// c. If the size of both heaps is the same. Then check if the current is less than the previous median or not. 
+//    If the current element is less than the previous median then insert it to the max heap and a new median will be equal to the top element of max heap. 
+//    If the current element is greater than the previous median then insert it to min-heap and new median will be equal to the top element of min heap.
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
