9ad7fa92d364b222655b150eb2b3c385d5860d3b
vedGit77 <104839104+vedGit77@users.noreply.github.com>
2022-06-03 12:46:54 +0530

    Update 13 Median of a Stream.cpp

--- 13 Median of a Stream.cpp
+++ 13 Median of a Stream.cpp
@@ -16,15 +16,15 @@
 
 //Efficient soln: Time= O(n* log n)
 
-// 1. Create two heaps. One max heap to maintain elements of lower half and one min heap to maintain elements of higher half at any point of time..
+// 1. Create two heaps. One MAX heap to maintain elements of LOWER half and one MIN heap to maintain elements of HIGHER half at any point of time.
 // 2. Take initial value of median as 0.
 // 3. For every newly read element, insert it into either max heap or min-heap and calculate the median based on the following conditions: 
-// a. If the size of max heap is greater than the size of min-heap and the element is less than the previous median 
-//    then pop the top element from max heap and insert into min-heap and insert the new element to max heap else insert the new element to min-heap. 
+// a. If the size of max heap is greater than the size of min-heap AND the element is less than the previous median, then pop the top element from max heap and insert into min-heap and insert the new element to max heap 
+//    else insert the new element to min-heap. 
 //    Calculate the new median as the average of top of elements of both max and min heap.
-// b. If the size of max heap is less than the size of min-heap and the element is greater than the previous median 
-//    then pop the top element from min-heap and insert into the max heap and insert the new element to min heap 
-//    else insert the new element to the max heap. Calculate the new median as the average of top of elements of both max and min heap.
+// b. If the size of max heap is less than the size of min-heap and the element is greater than the previous median, then pop the top element from min-heap and insert into the max heap and insert the new element to min heap 
+//    else insert the new element to the max heap. 
+//    Calculate the new median as the average of top of elements of both max and min heap.
 // c. If the size of both heaps is the same. Then check if the current is less than the previous median or not. 
 //    If the current element is less than the previous median then insert it to the max heap and a new median will be equal to the top element of max heap. 
 //    If the current element is greater than the previous median then insert it to min-heap and new median will be equal to the top element of min heap.
